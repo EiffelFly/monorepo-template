00b2b3fdd21485b80248faaff5e6d1c80128c69f
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-05-30 11:24:38 +0800

    feat: update readme and port

--- apps/docs/src/app/page.tsx
+++ apps/docs/src/app/page.tsx
@@ -7,7 +7,7 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <main className="mx-auto w-auto px-4 pt-16 pb-8 sm:pt-24 lg:px-8">
+      <main className="mx-auto w-auto px-4 pt-16 pb-8 sm:pt-24 lg:px-8 text-white">
         Hello World
       </main>
     </div>
